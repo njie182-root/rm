f4d4516bc8b299598fdcd24f59ef8f001c89ea17
Aji permana sidik <ajipermanasidik37@gmail.com>
2026-01-06 10:47:24 +0700

    Update generate-sitemap.js

--- scripts/generate-sitemap.js
+++ scripts/generate-sitemap.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 const path = require("path");
 
-const SITE_URL = "https://sunshinecaferesto.com"; // GANTI INI
+const SITE_URL = "https://sunshinecaferesto.com"; // INIURLNYA
 const ROOT_DIR = path.join(__dirname, "..");
 
 const ignore = ["node_modules", ".git", ".github", "scripts"];
